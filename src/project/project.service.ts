68aed44811f9e892ac7117eae761ef62916355f6
Joseph Harrison-Lim <joseph@pixleeteam.com>
2020-11-21 19:37:46 -0500

    Connect project service

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
+import { Project } from './entities/project.entity';
 
 @Injectable()
 export class ProjectService {
+  constructor(
+    @InjectRepository(Project)
+    private readonly projectRepository: Repository<Project>
+  ) {}
+
   create(createProjectDto: CreateProjectDto) {
-    return 'This action adds a new project';
+    return this.projectRepository.save(createProjectDto);
   }
 
   findAll() {
-    return `This action returns all project`;
+    return this.projectRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} project`;
+    return this.projectRepository.findOne(id);
   }
 
   update(id: number, updateProjectDto: UpdateProjectDto) {
-    return `This action updates a #${id} project`;
+    return this.projectRepository.update(id, updateProjectDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} project`;
+    return this.projectRepository.delete(id);
   }
 }
